aa8a98843e78e138027186d04993d0a8cb2cade4
zammanq <zpq5035@psu.edu>
2026-02-17 10:48:25 -0500

    Add toggle functionality for About section

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 console.log("JS connected");
 <script src="main.js"></script>
+<button id="toggleAbout">Toggle About</button>
+const aboutBtn = document.getElementById("toggleAbout");
+const aboutSection = document.getElementById("about");
+
+aboutBtn.addEventListener("click", () => {
+  aboutSection.classList.toggle("hidden");
+});
+.hidden {
+  display: none;
+}
